b97371d8aacba2f3e0bba079c76d5d438e2ec43a
kobayashi <shoji.kobayashi.0125@gmail.com>
2015-11-19 21:40:56 +0900

    add OopCalclator

---
+++ OopCalculatorSample/app/src/main/java/jp/freks/oopcalculatorsample/CalcException.java
@@ -0,0 +1,5 @@
+package jp.freks.oopcalculatorsample;
+
+public class CalcException extends Exception {
+    private static final long serialVersionUID = 1L;
+}
---
+++ OopCalculatorSample/app/src/main/java/jp/freks/oopcalculatorsample/Operation.java
@@ -0,0 +1,27 @@
+package jp.freks.oopcalculatorsample;
+
+public enum Operation {
+    PLUS {
+        double eval(double x, double y) {
+            return x + y;
+        }
+    },
+    MINUS {
+        double eval(double x, double y) {
+            return x - y;
+        }
+    },
+    TIMES {
+        double eval(double x, double y) {
+            return x * y;
+        }
+    },
+    DIVIDE {
+        double eval(double x, double y) {
+            return x / y;
+        }
+    };
+
+    abstract double eval(double x, double y);
+
+}
