85cd07048cdc5ca47680830862b764e02bce31cf
kobayashi <shoji.kobayashi.0125@gmail.com>
2015-11-28 14:50:45 +0900

    add saving file to internal storage

--- SaveFileSample/app/src/main/java/freks/jp/savefilesample/MainActivity.java
+++ SaveFileSample/app/src/main/java/freks/jp/savefilesample/MainActivity.java
@@ -1,7 +1,13 @@
 package freks.jp.savefilesample;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button internalButton = (Button)findViewById(R.id.internal_button);
+        internalButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                outputFile();
+            }
+        });
+
+    }
+
+    private void outputFile() {
+        String filename = "myfile.txt";
+        String string = "Hello world!";
+        FileOutputStream outputStream;
+
+        try {
+            outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+            outputStream.write(string.getBytes());
+            outputStream.close();
+
+            showSuccess();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void showSuccess() {
+        Toast.makeText(MainActivity.this, "suceess", Toast.LENGTH_LONG).show();
     }
 }
