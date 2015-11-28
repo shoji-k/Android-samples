ef9d2744f0fbe3f960155a782790ddb8cc251c25
kobayashi <shoji.kobayashi.0125@gmail.com>
2015-11-28 15:11:26 +0900

    add saving file to external storage

--- SaveFileSample/app/src/main/java/freks/jp/savefilesample/MainActivity.java
+++ SaveFileSample/app/src/main/java/freks/jp/savefilesample/MainActivity.java
@@ -1,13 +1,17 @@
 package freks.jp.savefilesample;
 
 import android.content.Context;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +28,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Button externalButton = (Button)findViewById(R.id.external_button);
+        externalButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                outputFileToExternalStorage();
+            }
+        });
+
     }
 
     private void outputFile() {
@@ -36,13 +48,56 @@ public class MainActivity extends AppCompatActivity {
             outputStream.write(string.getBytes());
             outputStream.close();
 
-            showSuccess();
+            show("success");
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private void showSuccess() {
-        Toast.makeText(MainActivity.this, "suceess", Toast.LENGTH_LONG).show();
+    private void outputFileToExternalStorage() {
+        if (! isExternalStorageWritable()) {
+            show("can not write");
+        } else {
+            File directory = getStorageDirectory(MainActivity.this, "sample");
+            File file = new File(directory, "test.txt");
+            FileWriter fw = null;
+            try {
+                fw = new FileWriter(file, true);
+                fw.write("sample");
+                fw.close();
+                fw = null;
+
+                show("create " + file.getAbsolutePath());
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (fw != null) {
+                    try {
+                        fw.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    private boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        return Environment.MEDIA_MOUNTED.equals(state);
+    }
+
+    private File getStorageDirectory(Context context, String directory) {
+        // Get the directory for the app's private pictures directory.
+        File file = new File(context.getExternalFilesDir(null), directory);
+        if (!file.mkdirs()) {
+            show("can not mkdir");
+        }
+        return file;
+    }
+
+
+    private void show(String message) {
+        Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
     }
 }
